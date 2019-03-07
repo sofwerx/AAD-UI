9249662e88c9c5c494a619e79241665508dc644f
Peter W. Purcell <peterwpurcell@gmail.com>
2019-03-06 23:01:41 -0600

    Add PropType Validation to DashboardItem

--- src/components/Dashboard/DashboardItem.js
+++ src/components/Dashboard/DashboardItem.js
@@ -3,24 +3,25 @@ import '../../assets/css/dashboard.css';
 import { Link } from 'react-router-dom';
 
 const PropTypes = require('prop-types');
-
-const DashboardItem = (props) => {
-  return (
-    <div className="dashboardItem">
-      <Link to={props.to}>
-        <div className="dashboard-block-wrapper">
-          <img alt="icon" src={props.iconLoc} width="100px"/>
-          <div className="j-title">{props.title}</div>
-          <hr className="thin width40Per"/>
-          <p className="dashboard-description-text">
-            {props.description}
-          </p>
-          <hr className="thin width40Per"/>
-        </div>
-      </Link>
-    </div>
-  );
-};
+class DashboardItem extends React.Component {
+  render() {
+    return (
+      <div className="dashboardItem">
+        <Link to={this.props.to}>
+          <div className="dashboard-block-wrapper">
+            <img alt="icon" src={this.props.iconLoc} width="100px"/>
+            <div className="j-title">{this.props.title}</div>
+            <hr className="thin width40Per"/>
+            <p className="dashboard-description-text">
+              {this.props.description}
+            </p>
+            <hr className="thin width40Per"/>
+          </div>
+        </Link>
+      </div>
+    );
+  }
+}
 
 DashboardItem.propTypes = {
   description: PropTypes.string,
